ea82a0da22724dcc56cc7e6972fee1b54f5b6574
Vishnu Rajeevan <vishnu@localhost.localdomain>
2012-06-24 21:12:25 -0400

    displays state list in listview

--- src/com/vishnurajeevan/android/NextBusActivity.java
+++ src/com/vishnurajeevan/android/NextBusActivity.java
@@ -7,23 +7,26 @@ import java.util.Iterator;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Element;
 
-import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
 
-public class NextBusActivity extends Activity {
+public class NextBusActivity extends ListActivity {
 	private static final String TAG = NextBusActivity.class.getSimpleName();
-	ArrayList<Element> stateList;
+	ArrayList<String> stateList;
 	int stateCount;
-	
+
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		stateCount = 0;
-		stateList = new ArrayList<Element>();
 
 		setContentView(R.layout.main);
+
+		stateCount = 0;
+		stateList = new ArrayList<String>();
+
 
 		org.jsoup.nodes.Document stateSelection;
 
@@ -38,23 +41,36 @@ public class NextBusActivity extends Activity {
 			Element table = stateSelection.select("table[border=1]").first();
 			String tableText = table.toString();
 			Log.v(TAG,tableText);
-			
+
 			Iterator<Element> ite = table.select("tr").iterator();
 
 			while(ite.hasNext()){
-				stateList.add(ite.next());
-				
-				
-				Log.v(TAG,"Value "+stateCount+": " + stateList.get(stateCount).text());
-				
+				stateList.add(ite.next().text());
+
+
+				Log.v(TAG,"Value "+stateCount+": " + stateList.get(stateCount));
+
 				stateCount++;
 			}
+
+			ArrayAdapter<String> arrayAdapter =   new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, stateList);
+			setListAdapter(arrayAdapter);
+
+
 
 
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+
+		//		ArrayList<String> your_array_list = new ArrayList<String>();
+		//        your_array_list.add("foo");
+		//        your_array_list.add("bar");
+		//        // This is the array adapter, it takes the context of the activity as a first // parameter, the type of list view as a second parameter and your array as a third parameter
+		//        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, your_array_list);
+		//        Log.v(TAG,""+arrayAdapter.getItem(0));
+		//        setListAdapter(arrayAdapter); 
 
 	}
 }
